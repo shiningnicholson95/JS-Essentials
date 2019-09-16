fce04f1a2bb76d1cdd65373e3c00c04d71ed8937
ashish <abadoni47@gmail.com>
2019-09-16 09:43:13 +0530

    first commit, 2 files added

---
+++ mod-1.js
@@ -0,0 +1,10 @@
+//unary, boolean, empty values; type conversions
+// console.log(typeof 4);
+// console.log(typeof 'x');
+// console.log(typeof true);
+
+// js does string evaluation from left to right comparing unicodes with that particular word.
+console.log('a'<'A');// it says false because unicode value of a=127 which is bigger than unicode of A=65
+console.log('b'<'a');// same as above, will produce false on compilation.
+
+
